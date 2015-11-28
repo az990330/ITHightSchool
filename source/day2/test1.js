75001f3bd71ab602729b4397eb9e0653b8be84bb
az990330 <az990330@naver.com>
2015-11-28 12:30:56 +0900

    day2: javascript 기본학습

---
+++ source/day2/test1.js
@@ -0,0 +1,30 @@
+// Number
+var a = 10;
+var b = 0.1;
+var c = -10;
+
+// String
+var d = "Hello";
+var e = 'Hello';
+var test1 = "Hey, \"fuck\"";
+var test2 = 'Hey, "fuck"';
+
+// Boolean
+var f = true; 
+var g = false;
+
+// undefined
+var h; // 선언만
+
+// Null 
+// 아무것도 없음
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(f));
+console.log(typeof(g));
+console.log(typeof(h));
+console.log(typeof(i));
---
+++ source/day2/test2.js
@@ -0,0 +1,18 @@
+// 객체를 만들어 보기
+var a = {};
+a['name']='Kimjoeun';
+a['age']=30;
+
+var b = {'name' : 'Kimjoeun' ,'age' : 30};
+var c = {name: 'Kimjoeun', age: 30};
+var d = new Object();
+d['name']='Kimjoeun';
+d['age']=30;
+var d = {};
+d.name = 'Kimjoeun'
+d.age = 30;
+
+console.log(a); console.log(typeof(a));
+console.log(b); console.log(typeof(b));
+console.log(c); console.log(typeof(c));
+console.log(d); console.log(typeof(d));
